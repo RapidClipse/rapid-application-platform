377374d233a844d0be47ba9266162dca9264e21b
FH <f.habermann@xdev-software.de>
2018-12-05 08:58:06 +0100

    PersistValueFlag Update

--- server/core/src/software/xdev/rap/server/ui/persistence/PersistValueFlag.java
+++ server/core/src/software/xdev/rap/server/ui/persistence/PersistValueFlag.java
@@ -29,28 +29,28 @@ import com.vaadin.flow.component.ComponentUtil;
  * @author XDEV Software
  *
  */
-public class PersistValueFlag
+public final class PersistValueFlag
 {
 	public static void set(final Component component, final boolean flag)
 	{
 		ComponentUtil.setData(component,PersistValueFlag.class,flag ? TRUE : FALSE);
 	}
-	
-	
+
+
 	public static boolean get(final Component component)
 	{
 		final PersistValueFlag flag = ComponentUtil.getData(component,PersistValueFlag.class);
 		return flag != null ? flag.value : DEFAULT;
 	}
-
-	private final static boolean			DEFAULT	= false;
 	
+	private final static boolean			DEFAULT	= false;
+
 	private final static PersistValueFlag	TRUE	= new PersistValueFlag(true);
 	private final static PersistValueFlag	FALSE	= new PersistValueFlag(false);
-	
+
 	private final boolean					value;
-	
-	
+
+
 	private PersistValueFlag(final boolean value)
 	{
 		super();
