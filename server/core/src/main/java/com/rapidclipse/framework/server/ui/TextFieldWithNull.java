503e028e2563d5416fd4ad6b6e6523ab1bbfde78
RapidClipse <rapidclipse@xdev-software.de>
2019-02-20 13:00:46 +0100

    License Header Update

--- server/core/src/main/java/com/rapidclipse/framework/server/ui/TextFieldWithNull.java
+++ server/core/src/main/java/com/rapidclipse/framework/server/ui/TextFieldWithNull.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2013-2019 by XDEV Software, All Rights Reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     XDEV Software Corp. - initial API and implementation
+ */
 
 package com.rapidclipse.framework.server.ui;
 
