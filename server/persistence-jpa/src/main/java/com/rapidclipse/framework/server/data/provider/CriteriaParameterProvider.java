fb19f9bc968d1c333ee051f42431c1bee76e3071
RapidClipse <rapidclipse@xdev-software.de>
2019-03-04 12:39:43 +0100

    CriteriaParameterProvider Serializable

--- server/persistence-jpa/src/main/java/com/rapidclipse/framework/server/data/provider/CriteriaParameterProvider.java
+++ server/persistence-jpa/src/main/java/com/rapidclipse/framework/server/data/provider/CriteriaParameterProvider.java
@@ -1,6 +1,7 @@
 
 package com.rapidclipse.framework.server.data.provider;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -13,103 +14,112 @@ import javax.persistence.TypedQuery;
  * @author XDEV Software
  *
  */
-public interface CriteriaParameterProvider
+public interface CriteriaParameterProvider extends Serializable
 {
 	public void setParameters(TypedQuery<?> query);
-
+	
 	public static CriteriaParameterProvider Empty()
 	{
 		return query -> {};
 	}
-
-	public static CriteriaParameterProvider ofSingleParameter(final String name, final Object value)
+	
+	public static CriteriaParameterProvider ofSingleParameter(final String name, final Serializable value)
 	{
 		return query -> query.setParameter(name, value);
 	}
-
-	public static <T, P> CriteriaParameterProvider ofSingleParameter(final Parameter<P> parameter, final P value)
+	
+	public static <T, P extends Serializable> CriteriaParameterProvider
+		ofSingleParameter(final Parameter<P> parameter, final P value)
 	{
 		return query -> query.setParameter(parameter, value);
 	}
-
-	public static CriteriaParameterProvider ofNamedMap(final Map<String, Object> parameterMap)
+	
+	public static CriteriaParameterProvider ofNamedMap(final Map<String, Serializable> parameterMap)
 	{
 		return query -> Static.setParametersNamedMap(query, parameterMap);
 	}
-
-	public static CriteriaParameterProvider ofParameterMap(final Map<Parameter<?>, Object> parameterMap)
+	
+	public static CriteriaParameterProvider ofParameterMap(final Map<Parameter<?>, Serializable> parameterMap)
 	{
 		return query -> Static.setParametersParameterMap(query, parameterMap);
 	}
-
+	
 	public static Builder Builder()
 	{
 		return new Builder.Implementation();
 	}
-
+	
 	public static interface Builder
 	{
-		public Builder add(String name, Object value);
-
-		public <P> Builder add(Parameter<P> parameter, P value);
-
+		public Builder add(String name, Serializable value);
+		
+		public <P extends Serializable> Builder add(Parameter<P> parameter, P value);
+		
 		public CriteriaParameterProvider build();
-
+		
 		public static class Implementation implements Builder
 		{
-			private final Map<String, Object>       nameToValue  = new HashMap<>();
-			private final Map<Parameter<?>, Object> paramToValue = new HashMap<>();
-
+			private final Map<String, Serializable>       nameToValue  = new HashMap<>();
+			private final Map<Parameter<?>, Serializable> paramToValue = new HashMap<>();
+			
 			@Override
-			public Builder add(final String name, final Object value)
+			public Builder add(final String name, final Serializable value)
 			{
 				this.nameToValue.put(name, value);
-
+				
 				return this;
 			}
-
+			
 			@Override
-			public <P> Builder add(final Parameter<P> parameter, final P value)
+			public <P extends Serializable> Builder add(final Parameter<P> parameter, final P value)
 			{
 				this.paramToValue.put(parameter, value);
-
+				
 				return this;
 			}
-
+			
 			@Override
 			public CriteriaParameterProvider build()
 			{
 				return query -> {
-
+					
 					Static.setParametersNamedMap(query, this.nameToValue);
 					Static.setParametersParameterMap(query, this.paramToValue);
 				};
 			}
 		}
 	}
-
+	
 	public static class Static
 	{
-		public static void setParametersNamedMap(final TypedQuery<?> query, final Map<String, Object> parameterMap)
+		public static void
+			setParametersNamedMap(final TypedQuery<?> query, final Map<String, Serializable> parameterMap)
 		{
-			for(final Entry<String, Object> entry : parameterMap.entrySet())
+			for(final Entry<String, Serializable> entry : parameterMap.entrySet())
 			{
 				final String name  = entry.getKey();
 				final Object value = entry.getValue();
 				query.setParameter(name, value);
 			}
 		}
-
+		
 		@SuppressWarnings({"rawtypes", "unchecked"})
 		public static void
-			setParametersParameterMap(final TypedQuery<?> query, final Map<Parameter<?>, Object> parameterMap)
+			setParametersParameterMap(
+				final TypedQuery<?> query,
+				final Map<Parameter<?>, Serializable> parameterMap)
 		{
-			for(final Entry<Parameter<?>, Object> entry : parameterMap.entrySet())
+			for(final Entry<Parameter<?>, Serializable> entry : parameterMap.entrySet())
 			{
 				final Parameter parameter = entry.getKey();
 				final Object    value     = entry.getValue();
 				query.setParameter(parameter, value);
 			}
+		}
+
+		private Static()
+		{
+			throw new Error();
 		}
 	}
 }
