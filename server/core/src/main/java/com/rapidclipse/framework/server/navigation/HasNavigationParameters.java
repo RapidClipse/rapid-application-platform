a09cb08d114c2a145c643f9e07564499130376c1
RapidClipse <info@rapidclipse.com>
2019-06-06 13:07:23 +0200

    Refactorings

--- server/core/src/main/java/com/rapidclipse/framework/server/navigation/HasNavigationParameters.java
+++ server/core/src/main/java/com/rapidclipse/framework/server/navigation/HasNavigationParameters.java
@@ -11,7 +11,13 @@
  * Contributors:
  *     XDEV Software Corp. - initial API and implementation
  */
+
 package com.rapidclipse.framework.server.navigation;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
@@ -26,7 +32,53 @@ public interface HasNavigationParameters extends HasUrlParameter<String>
 	@Override
 	default void setParameter(final BeforeEvent event, final String parameter)
 	{
-		NavigationUtils.injectParameters(this, event, parameter);
+		try
+		{
+			final NavigationParameterRegistry  registry = NavigationParameterRegistry.getCurrent();
+			final NavigationParameters         mapping  = registry.get(event.getLocation()
+				.getQueryParameters().getParameters().get(Navigation.ID_PARAMETER_NAME).get(0));
+			final NavigationParametersMetadata metadata = NavigationParametersMetadata
+				.New(this.getClass());
+			for(final String name : mapping.names())
+			{
+				final Object value  = mapping.value(name);
+				final Member member = metadata.get(name).member();
+				if(member instanceof Field)
+				{
+					final Field   field      = (Field)member;
+					final boolean accessible = field.isAccessible();
+					try
+					{
+						field.setAccessible(true);
+						field.set(this, value);
+					}
+					finally
+					{
+						field.setAccessible(accessible);
+					}
+				}
+				else
+				{
+					final Method  method     = (Method)member;
+					final boolean accessible = method.isAccessible();
+					try
+					{
+						method.setAccessible(true);
+						method.invoke(this, value);
+					}
+					finally
+					{
+						method.setAccessible(accessible);
+					}
+				}
+			}
+		}
+		catch(SecurityException | IllegalArgumentException | IllegalAccessException
+			| InvocationTargetException e)
+		{
+			throw new NavigationException(e);
+		}
+		
 		navigationParametersUpdated();
 	}
 	
--- server/core/src/main/java/com/rapidclipse/framework/server/navigation/NavigationParametersMetadata.java
+++ server/core/src/main/java/com/rapidclipse/framework/server/navigation/NavigationParametersMetadata.java
@@ -11,14 +11,22 @@
  * Contributors:
  *     XDEV Software Corp. - initial API and implementation
  */
+
 package com.rapidclipse.framework.server.navigation;
 
 import static java.util.Collections.unmodifiableMap;
 import static java.util.Objects.requireNonNull;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 
 
 /**
@@ -35,7 +43,78 @@ public interface NavigationParametersMetadata
 	
 	public static NavigationParametersMetadata New(final Class<?> targetType)
 	{
-		return New(NavigationUtils.getMetadata(targetType));
+		return New(getMetadata(targetType));
+	}
+
+	public static Map<String, NavigationParameterMetadata> getMetadata(final Class<?> target)
+	{
+		final List<Class<?>> hierarchy = new ArrayList<>();
+		Class<?>             clazz     = target;
+		while(clazz != Object.class)
+		{
+			hierarchy.add(0, clazz);
+			clazz = clazz.getSuperclass();
+		}
+
+		final Map<String, NavigationParameterMetadata> parameters = new HashMap<>();
+
+		for(final Class<?> current : hierarchy)
+		{
+			for(final Field field : current.getDeclaredFields())
+			{
+				if(Modifier.isStatic(field.getModifiers()))
+				{
+					continue;
+				}
+
+				final NavigationParameter urlParameter = field
+					.getAnnotation(NavigationParameter.class);
+				if(urlParameter == null)
+				{
+					continue;
+				}
+
+				String name = urlParameter.name();
+				if(StringUtils.isEmpty(name))
+				{
+					name = field.getName();
+				}
+				name = name.toLowerCase();
+				parameters.put(name, NavigationParameterMetadata.New(field, field.getType(), name,
+					urlParameter.optional()));
+			}
+
+			for(final Method method : current.getDeclaredMethods())
+			{
+				if(Modifier.isStatic(method.getModifiers())
+					|| method.getParameterTypes().length != 1)
+				{
+					continue;
+				}
+
+				final NavigationParameter urlParameter = method
+					.getAnnotation(NavigationParameter.class);
+				if(urlParameter == null)
+				{
+					continue;
+				}
+
+				String name = urlParameter.name();
+				if(StringUtils.isEmpty(name))
+				{
+					name = method.getName();
+					if(name.startsWith("set") && name.length() > 3)
+					{
+						name = name.substring(3);
+					}
+				}
+				name = name.toLowerCase();
+				parameters.put(name, NavigationParameterMetadata.New(method,
+					method.getParameterTypes()[0], name, urlParameter.optional()));
+			}
+		}
+
+		return parameters;
 	}
 	
 	public static NavigationParametersMetadata New(
