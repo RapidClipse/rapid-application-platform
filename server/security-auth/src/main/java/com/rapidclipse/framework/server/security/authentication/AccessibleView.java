fdbbcafaf914654aad0e4b10fdfcaac5e1a5beeb
RapidClipse <info@rapidclipse.com>
2019-09-12 13:35:27 +0200

    License Update

--- server/security-auth/src/main/java/com/rapidclipse/framework/server/security/authentication/AccessibleView.java
+++ server/security-auth/src/main/java/com/rapidclipse/framework/server/security/authentication/AccessibleView.java
@@ -1,17 +1,26 @@
 /*
  * Copyright (C) 2013-2019 by XDEV Software, All Rights Reserved.
  *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
+ * This file is part of the RapidClipse Application Platform (RAP).
  *
- * SPDX-License-Identifier: EPL-2.0
+ * RAP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RAP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  *
  * Contributors:
  *     XDEV Software Corp. - initial API and implementation
  */
-
 package com.rapidclipse.framework.server.security.authentication;
 
 import java.lang.annotation.ElementType;
