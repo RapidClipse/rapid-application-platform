050ae92da28df9131f7b06324d4045f583df1b55
RapidClipse <info@rapidclipse.com>
2019-10-02 11:43:54 +0200

    XWS-1710

--- server/core/src/main/java/com/rapidclipse/framework/server/ui/navigation/NavigationItemComponentFactory.java
+++ server/core/src/main/java/com/rapidclipse/framework/server/ui/navigation/NavigationItemComponentFactory.java
@@ -21,6 +21,7 @@
  * Contributors:
  *     XDEV Software Corp. - initial API and implementation
  */
+
 package com.rapidclipse.framework.server.ui.navigation;
 
 import java.io.Serializable;
@@ -47,53 +48,55 @@ public interface NavigationItemComponentFactory extends SerializableFunction<Nav
 	public static interface EventHandler extends Serializable
 	{
 		public void handleEvent(Component source, NavigationItem item);
-		
+
 		public static EventHandler Default()
 		{
 			return (source, item) -> UI.getCurrent().navigate(item.routeData().getNavigationTarget());
 		}
 	}
-
+	
 	public static NavigationItemComponentFactory LinkFactory()
 	{
 		return new LinkFactory();
 	}
-
+	
 	public static NavigationItemComponentFactory ButtonFactory()
 	{
 		return new ButtonFactory(EventHandler.Default());
 	}
-
+	
 	public static NavigationItemComponentFactory ButtonFactory(final EventHandler eventHandler)
 	{
 		return new ButtonFactory(eventHandler);
 	}
-
+	
 	public static class LinkFactory implements NavigationItemComponentFactory
 	{
 		protected LinkFactory()
 		{
 			super();
 		}
-
+		
 		@Override
 		public Component apply(final NavigationItem item)
 		{
-			return new RouterLink(item.displayName(), item.routeData().getNavigationTarget());
+			final RouterLink link = new RouterLink(item.displayName(), item.routeData().getNavigationTarget());
+			Optional.ofNullable(item.icon()).ifPresent(icon -> link.addComponentAsFirst(icon.get()));
+			return link;
 		}
 	}
-
+	
 	public static class ButtonFactory implements NavigationItemComponentFactory
 	{
 		private final EventHandler eventHandler;
-
+		
 		protected ButtonFactory(final EventHandler eventHandler)
 		{
 			super();
-
+			
 			this.eventHandler = Objects.requireNonNull(eventHandler);
 		}
-
+		
 		@Override
 		public Component apply(final NavigationItem item)
 		{
