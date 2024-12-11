0d5c514f0757c92688ee253296b701f3cd109419
JM <j.meissner@xdev-software.de>
2024-12-11 08:29:03 +0100

    XWS-2373 - Use hibernate's built-in copy/createCountQuery functionality to generate new CriteriaQuery from another one

--- rap-server-persistence-jpa/src/main/java/com/rapidclipse/framework/server/data/provider/CriteriaDataProvider.java
+++ rap-server-persistence-jpa/src/main/java/com/rapidclipse/framework/server/data/provider/CriteriaDataProvider.java
@@ -31,6 +31,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import org.hibernate.query.sqm.tree.SqmCopyContext;
+import org.hibernate.query.sqm.tree.select.SqmSelectStatement;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -53,7 +56,7 @@ import com.vaadin.flow.data.provider.SortDirection;
 
 /**
  * @author XDEV Software
- *
+ * 
  */
 public interface CriteriaDataProvider<T> extends FilterableDataProvider<T, Filter>
 {
@@ -61,109 +64,117 @@ public interface CriteriaDataProvider<T> extends FilterableDataProvider<T, Filte
 	{
 		return New(criteria, CriteriaParameterProvider.Empty());
 	}
-
-	public static <T> CriteriaDataProvider<T>
-		New(final CriteriaQuery<T> criteria, final CriteriaParameterProvider parameterProvider)
+	
+	public static <T> CriteriaDataProvider<T> New(final CriteriaQuery<T> criteria,
+		final CriteriaParameterProvider parameterProvider)
 	{
 		return new Default<>(criteria, parameterProvider);
 	}
-
+	
 	public static class Default<T>
 		extends ConfigurableFilterDataProviderWrapper<T, Filter, Filter, Filter>
 		implements CriteriaDataProvider<T>
 	{
 		protected Default(final CriteriaQuery<T> criteria, final CriteriaParameterProvider parameters)
 		{
-			super(DataProvider.fromFilteringCallbacks(new CriteriaFetchCallback<>(criteria, parameters),
-				new CriteriaCountCallback<>(criteria, parameters)));
+			super(DataProvider.fromFilteringCallbacks(new CriteriaFetchCallback<>(criteria,
+				parameters), new CriteriaCountCallback<>(criteria, parameters)));
 		}
-
+		
 		@Override
 		protected Filter combineFilters(final Filter queryFilter, final Filter configuredFilter)
 		{
-			if(queryFilter != null && configuredFilter != null)
+			if (queryFilter != null && configuredFilter != null)
 			{
 				return Filter.And(queryFilter, configuredFilter);
 			}
-
+			
 			return queryFilter != null ? queryFilter : configuredFilter;
 		}
-
+		
 		protected static abstract class CriteriaCallbackBase<T> implements Serializable
 		{
 			private final CriteriaQuery<T>          criteria;
 			private final CriteriaParameterProvider parameterProvider;
 			private final Root<?>                   root;
-
+			
 			protected CriteriaCallbackBase(
 				final CriteriaQuery<T> criteria,
 				final CriteriaParameterProvider parameterProvider)
 			{
 				super();
-
-				this.criteria          = requireNonNull(criteria);
+				
+				this.criteria = requireNonNull(criteria);
 				this.parameterProvider = requireNonNull(parameterProvider);
-				this.root              = Jpa.getRoot(criteria);
-				if(this.root == null)
+				this.root = Jpa.getRoot(criteria);
+				if (this.root == null)
 				{
 					throw new IllegalArgumentException("Unsupported criteria");
 				}
 			}
-
+			
 			protected CriteriaQuery<T> criteria()
 			{
 				return this.criteria;
 			}
-
+			
 			protected CriteriaParameterProvider parameterProvider()
 			{
 				return this.parameterProvider;
 			}
-
+			
 			protected Root<?> root()
 			{
 				return this.root;
 			}
-
+			
 			protected EntityManager entityManager()
 			{
 				return Jpa.getEntityManager(root().getJavaType());
 			}
-
-			protected CriteriaQuery<T> createCriteria(
+			
+			protected CriteriaQuery<T> createCriteria_applyVaadinQuery(
 				final Query<T, Filter> query,
 				final EntityManager entityManager)
 			{
 				final CriteriaQuery<T> originalCriteria = criteria();
 				final Root<?>          root             = root();
-
-				final CriteriaQuery<T> criteria = entityManager.getCriteriaBuilder()
-					.createQuery(originalCriteria.getResultType());
-				Jpa.copyCriteria(originalCriteria, criteria);
-
+				
+				CriteriaQuery<T>       copiedCriteria;
+				if (originalCriteria instanceof SqmSelectStatement)
+				{
+					copiedCriteria = ((SqmSelectStatement<T>) this.criteria()).copy(SqmCopyContext.simpleContext());
+				}
+				else
+				{
+					copiedCriteria = entityManager.getCriteriaBuilder()
+						.createQuery(originalCriteria.getResultType());
+					Jpa.copyCriteria(originalCriteria, copiedCriteria);
+				}
+				
 				final Optional<Filter> optFilter = query.getFilter();
-				if(optFilter.isPresent())
+				if (optFilter.isPresent())
 				{
 					final Filter    filter              = optFilter.get();
-					Predicate       predicate           = new CriteriaFilterConverter<>(criteria).apply(filter);
+					Predicate       predicate           = new CriteriaFilterConverter<>(copiedCriteria).apply(filter);
 					final Predicate originalWhereClause = originalCriteria.getRestriction();
-					if(originalWhereClause != null)
+					if (originalWhereClause != null)
 					{
 						predicate = entityManager.getCriteriaBuilder().and(predicate, originalWhereClause);
 					}
-					criteria.where(predicate);
+					copiedCriteria.where(predicate);
 				}
-
+				
 				final List<QuerySortOrder> sortOrders = query.getSortOrders();
-				if(sortOrders != null && !sortOrders.isEmpty())
+				if (sortOrders != null && !sortOrders.isEmpty())
 				{
 					final List<Order> orders = new ArrayList<>();
-					for(final QuerySortOrder sortOrder : sortOrders)
+					for (final QuerySortOrder sortOrder : sortOrders)
 					{
 						final Path<?> path = Jpa.resolvePath(root, sortOrder.getSorted());
-						if(path != null)
+						if (path != null)
 						{
-							if(sortOrder.getDirection() == SortDirection.ASCENDING)
+							if (sortOrder.getDirection() == SortDirection.ASCENDING)
 							{
 								orders.add(entityManager.getCriteriaBuilder().asc(path));
 							}
@@ -174,13 +185,13 @@ public interface CriteriaDataProvider<T> extends FilterableDataProvider<T, Filte
 						}
 					}
 					orders.addAll(originalCriteria.getOrderList());
-					criteria.orderBy(orders);
+					copiedCriteria.orderBy(orders);
 				}
-
-				return criteria;
+				
+				return copiedCriteria;
 			}
 		}
-
+		
 		protected static class CriteriaFetchCallback<T> extends CriteriaCallbackBase<T>
 			implements FetchCallback<T, Filter>
 		{
@@ -190,23 +201,23 @@ public interface CriteriaDataProvider<T> extends FilterableDataProvider<T, Filte
 			{
 				super(criteria, parameterProvider);
 			}
-
+			
 			@Override
 			public Stream<T> fetch(final Query<T, Filter> query)
 			{
-				final EntityManager entityManager = entityManager();
-
-				final CriteriaQuery<T> criteria = createCriteria(query, entityManager);
-
-				final TypedQuery<T> typedQuery = entityManager.createQuery(criteria);
+				final EntityManager    entityManager  = entityManager();
+				
+				final CriteriaQuery<T> copiedCriteria = createCriteria_applyVaadinQuery(query, entityManager);
+				
+				final TypedQuery<T>    typedQuery     = entityManager.createQuery(copiedCriteria);
 				parameterProvider().setParameters(typedQuery);
 				typedQuery.setFirstResult(query.getOffset());
 				typedQuery.setMaxResults(query.getLimit());
-
+				
 				return typedQuery.getResultList().stream();
 			}
 		}
-
+		
 		protected static class CriteriaCountCallback<T> extends CriteriaCallbackBase<T>
 			implements CountCallback<T, Filter>
 		{
@@ -216,14 +227,24 @@ public interface CriteriaDataProvider<T> extends FilterableDataProvider<T, Filte
 			{
 				super(criteria, parameterProvider);
 			}
-
+			
 			@Override
 			public int count(final Query<T, Filter> query)
 			{
-				final EntityManager       entityManager = entityManager();
-				final CriteriaQuery<T>    criteria      = createCriteria(query, entityManager);
-				final CriteriaQuery<Long> countCriteria = Jpa.countCriteria(criteria, entityManager);
-				final TypedQuery<Long>    typedQuery    = entityManager.createQuery(countCriteria);
+				final EntityManager    entityManager  = entityManager();
+				final CriteriaQuery<T> copiedCriteria = this.createCriteria_applyVaadinQuery(query, entityManager);
+				
+				CriteriaQuery<Long>    countCriteria;
+				if (copiedCriteria instanceof SqmSelectStatement)
+				{
+					countCriteria = ((SqmSelectStatement<T>) copiedCriteria).createCountQuery();
+				}
+				else
+				{
+					countCriteria = Jpa.countCriteria(copiedCriteria, entityManager);
+				}
+				
+				final TypedQuery<Long> typedQuery = entityManager.createQuery(countCriteria);
 				parameterProvider().setParameters(typedQuery);
 				return typedQuery.getSingleResult().intValue();
 			}
